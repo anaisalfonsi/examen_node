ac172610b5055f1d79853aab99e48c30cd0fe59d
anaisalfonsi <alfonsi_anais@hotmail.fr>
2022-09-02 17:29:43 +0200

    fini

--- app/server.js
+++ app/server.js
@@ -1,13 +1,26 @@
 import express from "express";
-import router from "./router.js";
-
+import session from "express-session";
+import userRouter from "./routers/user.js";
+import loginRouter from "./routers/login.js";
 
 const app = express();
 const port = 3002;
 
 app.set("view engine", "pug");
 
-app.use("/", router);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    name: "login",
+    secret: "alklkdin987hdjd",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use("/", userRouter, loginRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
